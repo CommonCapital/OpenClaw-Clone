04ea203dbb0efe3dbe2a27f4c0058fc64e68cadf
CommonCapital <nursan2007@gmail.com>
2026-03-03 16:56:03 +0500

    add

---
+++ lib/auth.ts
@@ -0,0 +1,31 @@
+import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
+import { cache } from "react"
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "./drizzle"; // your drizzle db config
+import { headers } from "next/headers";
+export const auth = betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg", // or "pg" or "mysql"
+    }),
+   emailAndPassword: {
+     enabled: true,
+     plugins: [
+        nextCookies(),
+    ],
+    socialProviders: {
+      google: {
+           clientId: process.env.GOOGLE_CLIENT_ID!,
+           clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      },
+      linkedin: {
+            clientId: process.env.LINKEDIN_CLIENT_ID!,
+            clientSecret: process.env.LINKEDIN_CLIENT_SECRET!,
+      },
+    }
+}});
+export const getSession = cache(async () => {
+      return await auth.api.getSession({
+         headers: await headers()
+     })
+ })
